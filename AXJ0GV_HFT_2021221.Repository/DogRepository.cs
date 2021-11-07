6d64188f4eec776c0bc451b203cf36305399c034
Tolnai Dominik - AXJ0GV <tolnai.dominik@stud.uni-obuda.hu>
2021-11-07 21:35:25 +0100

    Dog & Owner repository created and finished w/ CRUD.

---
+++ AXJ0GV_HFT_2021221.Repository/DogRepository.cs
@@ -0,0 +1,55 @@
+﻿using AXJ0GV_HFT_2021221.Data;
+using AXJ0GV_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AXJ0GV_HFT_2021221.Repository
+{
+    class DogRepository
+    {
+        DogDbContext context;
+
+        public DogRepository(DogDbContext context)
+        {
+            this.context = context;
+        }
+        public void Create(Dog dog)
+        {
+            context.Dogs.Add(dog);
+            context.SaveChanges();
+        }
+        public void Delete(int dogID)
+        {
+            context.Dogs.Remove(ReadOne(dogID));
+            context.SaveChanges();
+        }
+
+        public IQueryable<Dog> ReadAll()
+        {
+            return context.Dogs;
+        }
+
+        public Dog ReadOne(int id)
+        {
+            return context
+                .Dogs
+                .FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Dog dog)
+        {
+            Dog old = ReadOne(dog.Id);
+
+            old.Injections = dog.Injections;
+            old.Name = dog.Name;
+            old.OwnerID = dog.OwnerID;
+            old.Sex = dog.Sex;
+            old.Species = dog.Species;
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ AXJ0GV_HFT_2021221.Repository/OwnerRepository.cs
@@ -0,0 +1,56 @@
+﻿using AXJ0GV_HFT_2021221.Data;
+using AXJ0GV_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AXJ0GV_HFT_2021221.Repository
+{
+    class OwnerRepository
+    {
+        DogDbContext context;
+
+        public OwnerRepository(DogDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(Owner owner)
+        {
+            context.Owners.Add(owner);
+            context.SaveChanges();
+        }
+        public void Delete(int objid)
+        {
+            context.Owners.Remove(ReadOne(objid));
+
+            context.SaveChanges();
+        }
+
+        public IQueryable<Owner> ReadAll()
+        {
+            return context.Owners;
+        }
+
+        public Owner ReadOne(int id)
+        {
+            return context
+                .Owners
+                .FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Owner owner)
+        {
+            Owner old = ReadOne(owner.Id);
+
+            old.Name = owner.Name;
+            old.IdentityCardNumber = owner.IdentityCardNumber;
+            old.Sex = owner.Sex;
+            old.Dogs = owner.Dogs;
+            
+            context.SaveChanges();
+        }
+    }
+}
