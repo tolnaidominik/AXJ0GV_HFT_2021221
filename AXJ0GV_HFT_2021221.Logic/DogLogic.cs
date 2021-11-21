1769faa11a2d56f57330dac55fa3900f0f0434d8
Tolnai Dominik - AXJ0GV <tolnai.dominik@stud.uni-obuda.hu>
2021-11-21 20:30:57 +0100

    DogLogic CRUD created and finished

--- AXJ0GV_HFT_2021221.Logic/DogLogic.cs
+++ AXJ0GV_HFT_2021221.Logic/DogLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AXJ0GV_HFT_2021221.Models;
+using AXJ0GV_HFT_2021221.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,31 @@ namespace AXJ0GV_HFT_2021221.Logic
 {
     class DogLogic
     {
-    
+        DogRepository repo;
+
+        public DogLogic(DogRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Dog dog)
+        {
+            repo.Create(dog);
+        }
+
+        public void Delete(int dogId)
+        {
+            repo.Delete(dogId);
+        }
+
+        public IQueryable<Dog> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(Dog dog)
+        {
+            repo.Update(dog);
+        }
     }
 }
