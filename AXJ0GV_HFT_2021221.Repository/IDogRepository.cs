c7cb1578ac0392203aae548ad6f9925274fe719e
Tolnai Dominik - AXJ0GV <tolnai.dominik@stud.uni-obuda.hu>
2021-11-21 20:37:02 +0100

    IDogRepo & IInjectionRepo created and finished

---
+++ AXJ0GV_HFT_2021221.Repository/IDogRepository.cs
@@ -0,0 +1,18 @@
+﻿using AXJ0GV_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AXJ0GV_HFT_2021221.Repository
+{
+    interface IDogRepository
+    {
+        void Create(Dog dog);
+        Dog ReadOne(int id); // GetById
+        IQueryable<Dog> ReadAll(); // Query
+        void Update(Dog car);
+        void Delete(int carId);
+    }
+}
---
+++ AXJ0GV_HFT_2021221.Repository/IInjectionRepository.cs
@@ -0,0 +1,18 @@
+﻿using AXJ0GV_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AXJ0GV_HFT_2021221.Repository
+{
+    interface IInjectionRepository
+    {
+        void Create(Injection injection);
+        Injection ReadOne(int id); // GetById
+        IQueryable<Injection> ReadAll(); // Query
+        void Update(Injection car);
+        void Delete(int carId);
+    }
+}
