1f3aef44d39952959d7690e6ebfcfc5bd793becd
Tolnai Dominik <tolnai.dominik@stud.uni-obuda.hu>
2021-12-02 19:19:33 +0100

    Endpoint Startup & Program classes checked and finished.

--- AXJ0GV_HFT_2021221.Endpoint/Startup.cs
+++ AXJ0GV_HFT_2021221.Endpoint/Startup.cs
@@ -1,6 +1,9 @@
+using AXJ0GV_HFT_2021221.Data;
+using AXJ0GV_HFT_2021221.Logic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +15,23 @@ namespace AXJ0GV_HFT_2021221.Endpoint
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddTransient<IDogLogic, DogLogic>();
+            services.AddTransient<IOwnerLogic, OwnerLogic>();
+            services.AddTransient<IInjectionLogic, InjectionLogic>();
+            services.AddTransient<DogDbContext, DogDbContext>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +41,16 @@ namespace AXJ0GV_HFT_2021221.Endpoint
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
